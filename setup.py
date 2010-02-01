65eb4de8cb44e02542e74f7c3a5cf884fd867106
Jose Soares <jsoares@adv1179.nwc.newsworld.net>
2010-02-01 11:05:02 -0500

    Added app with setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='offensivecontent',
+      version='.1',
+      description='',
+      long_description='',
+      author='',
+      author_email='',
+      url='http://opensource.washingtontimes.com/projects/offensivecontent/',
+      packages=['offensivecontent'],
+      classifiers=['Framework :: Django',
+          'License :: OSI Approved :: Apache Software License',
+          'Development Status :: 4 - Beta',
+          'Environment :: Other Environment',
+          'Programming Language :: Python',
+          ],
+      )
