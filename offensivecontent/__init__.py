4636c6628a1be0269aee522119f3969416812284
Corey Oordt <coordt@washingtontimes.com>
2010-04-28 13:44:54 -0400

    Updated version

--- offensivecontent/__init__.py
+++ offensivecontent/__init__.py
@@ -1,9 +1,9 @@
 __version_info__ = {
     'major': 0,
-    'minor': 1,
+    'minor': 2,
     'micro': 0,
-    'releaselevel': 'beta',
-    'serial': 5
+    'releaselevel': 'final',
+    'serial': 0
 }
 
 def get_version():
